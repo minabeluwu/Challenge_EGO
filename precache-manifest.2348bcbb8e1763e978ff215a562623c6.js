2d61d88b111f48dac4c6ace3301130471a6bd61f
Minabeluwu <minabeluwu@gmail.com>
2020-05-12 11:06:35 -0300

    Updates

---
+++ precache-manifest.2348bcbb8e1763e978ff215a562623c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90d2b119efb93395fa7dd30acc1b0c81",
+    "url": "/Challenge_EGO/index.html"
+  },
+  {
+    "revision": "e1b59eb462e0d31420cc",
+    "url": "/Challenge_EGO/static/css/main.488924c5.chunk.css"
+  },
+  {
+    "revision": "9557d7722bf26fa71664",
+    "url": "/Challenge_EGO/static/js/2.87dc0e79.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Challenge_EGO/static/js/2.87dc0e79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1b59eb462e0d31420cc",
+    "url": "/Challenge_EGO/static/js/main.badeef45.chunk.js"
+  },
+  {
+    "revision": "36750f103ece8b7bcd26",
+    "url": "/Challenge_EGO/static/js/runtime-main.63f8ada9.js"
+  }
+]);
